aa57bc20cab23eb0ac6070d29f4843b780c06989
Levi <levi-sr@outlook.com>
2020-09-20 19:36:50 -0300

    React Router

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="ui raised very padded text container segment"
+            style={{ marginTop: '80px' }}>
+            <h3 className='ui header'>Home</h3>
+            <p>lorem ipsum dolor sit ammet, consectur adispisciting</p>
+        </div>
+
+    )
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <nav className="ui raised very padded segment">
+            <a href="#" className="ui teal interverted segment">Gloria</a>
+            <div className="ui right floated header">
+                <button className="ui button"><a href="/">Home</a></button>
+                <button className="ui button"><a href="/about">About</a></button>
+                <button className="ui button"><a href="/contact">Contact</a></button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
